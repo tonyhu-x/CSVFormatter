5efd4819f92780c6442f3b6aeb1d8b1068a2de9c
Tony (Juntao) Hu <807769469hjt@gmail.com>
2020-08-20 23:57:31 -0400

    Add simple printout functionality

--- main.py
+++ main.py
@@ -1,28 +1,70 @@
 """This is a CSV formatter."""
 
+import argparse
+import sys
+
 MAX_LINE_COUNT = 1024
 MAX_LINE_WIDTH = 512
+DEFAULT_DISPLAY_LINE_COUNT = 10
 
 
 def read(file_name: str) -> list:
     """Read the contents of the file."""
     lst = list()
+    col_count = -1
 
     with open(file_name, 'r') as f:
-        for _ in range(MAX_LINE_COUNT):
-            line = f.readline()
-            if line == '':  # we have reached EOF
-                break
-            elif not line.strip():  # if line contains only whitespace
-                continue
-            if len(line) > MAX_LINE_WIDTH:
-                raise RuntimeError('Line too long!')
-            lst.append(line)
-        if f.readline != '':
-            raise RuntimeError('File contains too many lines')
+        try:
+            for i in range(MAX_LINE_COUNT):
+                line = f.readline()
+                if line == '':  # we have reached EOF
+                    break
+                line = line.strip()
+                if not line:  # if line contains only whitespace
+                    continue
+                if len(line) > MAX_LINE_WIDTH:
+                    raise RuntimeError('Line too long!')
+                temp_lst = line.split(',')
+                temp_col_count = len(temp_lst)
+                if col_count == -1:
+                    col_count = temp_col_count
+                elif col_count != temp_col_count:
+                    raise RuntimeError(
+                        f'Error in line {i + 1}: expected {col_count} column(s) but found {temp_col_count}')
+                lst.append(temp_lst)
+            if f.readline() != '':
+                raise RuntimeError('File contains too many lines')
+        except RuntimeError as e:
+            print(str(e))
+            exit(1)
 
     return lst
 
-def format(lst: list):
-    nlst = list()
-    
+
+def print_formatted(contents: list):
+    """Print the contents."""
+    lengths = [-1] * len(contents[0])
+
+    for i in range(len(lengths)):
+        for j in contents:
+            lengths[i] = max(len(j[i]), lengths[i])
+
+    for ln in contents:
+        for i, tok in enumerate(ln):
+            print(tok + ' ' * (lengths[i] - len(tok)), end='')
+            if i != len(ln) - 1:
+                print(',', end='')
+        print()
+
+
+def main(args: argparse.Namespace):
+    """This is the main function."""
+    contents = read(args.file_name)
+    print_formatted(contents)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='A command line CSV viewer and formatter.')
+    parser.add_argument('-l', action='store_true', help='specify the number of lines to display on each screen')
+    parser.add_argument('file_name')
+    args = parser.parse_args()
+    main(args)  # remove the name argument
